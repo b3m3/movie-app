661c39dc6c9bc662cf1f7ac5c1d8309c3b29333b
b3m3 <b3m3@yandex.ru>
2022-08-27 17:26:31 +0300

    start

---
+++ src/constans/api.js
@@ -0,0 +1,17 @@
+const MOVIEDB_ROOT = 'https://api.themoviedb.org/3/';
+const MOVIEDB_API = '?api_key='+process.env.REACT_APP_WEATHER_API_KEY;
+const MOVIEDB_LANG = '&language=';
+const MOVIEDB_PAGE = '/&page=';
+
+const MOVIEDB_MOVIES = 'movie/';
+const MOVIEDB_TV = 'tv/';
+
+// categoryes
+const MOVIEDB_LATEST = 'latest';
+const MOVIEDB_POPULAR = 'popular';
+const MOVIEDB_TOP_RATED = 'top_rated';
+const MOVIEDB_NOW_PLAYUNG = 'now_playing';
+
+// images
+const MOVIEDB_POSTER_S = 'https://image.tmdb.org/t/p/w500/';
+const MOVIEDB_POSTER_B = 'https://image.tmdb.org/t/p/original/';
