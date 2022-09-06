2b5ef67b518f02c9f7d5e5c311797b9b69c30b86
b3m3 <uwm.d.c.a@gmail.com>
2022-09-06 16:58:19 +0300

    src/routes/routesConfig

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -1,32 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from '../../components/header/Header';
-import Home from '../home/Home';
-import Movies from '../movies/Movies';
-import Series from '../series/Series';
-import Category from '../category/Category';
-import Info from '../info/Info';
-import NotFound from '../notFound/NotFound';
 
-import { PAGE_ROOT } from '../../constans/api';
+import routesConfig from '../../routes/routesConfig';
 
 import './app.css';
 
 const App = () => {
-
   return (
     <div className="app">
       <BrowserRouter>
         <Header />
         <main>
           <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/movie' element={<Movies />}></Route>
-            <Route path='/tv' element={<Series />}></Route>
-
-            <Route path='/:tv/:id' element={<Info />}></Route>
-            <Route path={`/:tv/:category${PAGE_ROOT}:id`} element={<Category />}></Route>
-            <Route path='*' element={<NotFound />}></Route>
+            {routesConfig.map(({ path, element }) => (
+                <Route path={path} element={element}/>))}
           </Routes>
         </main>
       </BrowserRouter>
--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,15 +1,37 @@
-import MoviesPage from "../containers/moviesPage/MoviesPage";
-import TvPage from "../containers/tvPage/TvPage";
+import Home from '../containers/home/Home';
+import Movies from '../containers/movies/Movies';
+import Series from '../containers/series/Series';
+import Category from '../containers/category/Category';
+import Info from '../containers/info/Info';
+import NotFound from '../containers/notFound/NotFound';
+
+import { PAGE_ROOT } from '../constans/api';
 
 const routesConfig = [
   {
-    path: "/MoviesPage",
-    element: <MoviesPage />
+    path: "/",
+    element: <Home />
   },
   {
-    path: "/TvPage",
-    element: <TvPage />
-  }
+    path: "/movie",
+    element: <Movies />
+  },
+  {
+    path: "/tv",
+    element: <Series />
+  },
+  {
+    path: "/:tv/:id",
+    element: <Info />
+  },
+  {
+    path: "/:tv/:category"+PAGE_ROOT+":id",
+    element: <Category />
+  },
+  {
+    path: "*",
+    element: <NotFound />
+  },
 ];
 
 export default routesConfig;
