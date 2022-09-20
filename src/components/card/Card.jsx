67ea48e525d8452d6d81a9c551eea1fc73f2931d
b3m3 <uwm.d.c.a@gmail.com>
2022-09-20 16:08:01 +0300

    Card - add and remove card to favorite

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,47 +1,64 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Poster from '../images/poster/Poster';
 import Rating from '../rating/Rating';
-import { roundNumber } from '../../utils/utils';
+import { setMovieToFavorite, removeMovieFromFavorite } from '../../store/actions/index';
+
+import { MdFavorite } from 'react-icons/md';
 
 import style from './card.module.css';
 
 const Card = ({ pathTv, id, poster_path, title, name, vote_average }) => {
-  const [color, setColor] = useState([]);
+  const [favorite, setFavorite] = useState(false);
+
+  const storeData = useSelector(store => store.favoriteReduces);
+  const dispatch = useDispatch();
+
+  const addMovie = () => {
+    dispatch(setMovieToFavorite({
+        [id]: { title, name, poster_path, vote_average }
+    }));
+  };
+
+  const removeMovie = () => {
+    dispatch(removeMovieFromFavorite(id));
+  };
 
   useEffect(() => {
-    setColor(
-      roundNumber(vote_average) >= 8 ? 'green' 
-      : roundNumber(vote_average) >= 7 ? 'yellowGreen' 
-      : roundNumber(vote_average) >= 6 ? 'yellow' 
-      : roundNumber(vote_average) >= 5 ? 'orange' 
-      : 'red');
-  }, [vote_average]);
-  
+    storeData[id] ? setFavorite(true) : setFavorite(false)
+  }, [favorite, storeData]);
+
   return (
-    <Link 
-      to={`/${pathTv}${id}`}
-      className={style.card}
-    >
+    <div className={style.card} >
       <span className={style.rating}>
         <Rating 
-          data={vote_average} 
-          color={color}
+          data={vote_average}
         />
       </span>
 
-      <Poster 
-        src={poster_path}
-        alt={title}
-        hover={true}
-        hoverColor={color}
-      />
+      <span
+        className={style.favorite}
+        style={favorite ? {color: 'orange'} : null}
+        onClick={favorite ? removeMovie : addMovie}
+      >
+        <MdFavorite />
+      </span>
+
+      <Link to={`/${pathTv}${id}`}>
+        <Poster 
+          src={poster_path}
+          alt={title}
+          hover={true}
+          vote_average={vote_average}
+        />
+      </Link>
 
       <p className={style.title}>
         {title && title} {name && name}
       </p> 
-    </Link>
+    </div>
   );
 }
 
