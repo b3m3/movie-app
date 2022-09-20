115397294d650c781d15c30ac1441777a272952c
b3m3 <uwm.d.c.a@gmail.com>
2022-09-20 16:06:04 +0300

    src/containers/favorite

---
+++ src/containers/favorite/Favorite.jsx
@@ -0,0 +1,48 @@
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+
+// import { MOVIES, SERIES } from '../../constans/api';
+import withList from '../../hoc/withList';
+import Card from '../../components/card/Card';
+import Title from '../../components/title/Title';
+
+import { MdFavorite } from 'react-icons/md';
+
+const Favorite = () => {
+  const [favoriteData, setFavoriteData] = useState(null);
+  const storeData = useSelector(store => store.favoriteReduces);
+
+  const CardList = withList(Card, favoriteData, 'grid');
+
+  useEffect(() => {
+    const arr = Object.entries(storeData);
+    const res = arr.map(item => ({
+      id: item[0],
+      ...item[1]
+    }))
+
+    setFavoriteData(res);
+  }, [storeData]);
+
+  return (
+    <div className="favorite">
+      <div className="container">
+        <Title 
+          title={'Favorite'}
+          icon={<MdFavorite />}
+          color={'orange'}
+        />
+        {favoriteData && favoriteData.length 
+          ? <CardList />
+          : <h4 
+              style={{textAlign: 'center', textShadow: '0 0 7px #fff', display: 'block'}}
+            >
+              Список пуст &#129335;
+            </h4>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Favorite;
